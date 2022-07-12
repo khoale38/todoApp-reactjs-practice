8d5bab054475a98db5acde33f2328d16e9723992
Khoale38 <kevinle38@gmail.com>
2022-07-12 17:23:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './component/TodoList'
+import Textfield from '@atlaskit/textfield'
+import Button from '@atlaskit/button'
+import { v4 } from 'uuid'
+import { useCallback, useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const TODO_APP_STORAGE_KEY = "TODO_APP";
+
+function App () {
+  const [todoList, setTodoList] = useState([])
+  const [textInput, setTextInput] = useState('')
+  const onTextInputChange = useCallback((e) => {
+    setTextInput(e.target.value)
+  }, [])
+  
+  const onAddBtnClick = useCallback((e) => {
+
+    setTodoList([...todoList, {id: v4(),name: textInput,isCompleted: false}])
+    setTextInput('')
+  }, [textInput, todoList])
+  
+
+  //Local Storage not working dont know why ?
+  useEffect(() => {
+    const storagedTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if (storagedTodoList) {
+      setTodoList(JSON.parse(storagedTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  }, [todoList]);
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  }, []);
+
+  
+    return (
+    <div>
+      <h3>Todo List</h3>
+      <Textfield
+        css={{padding: '2px 4px 2px'}}
+        onChange={onTextInputChange}
+        value={textInput}
+        name='add-todo'
+        placeholder='Thêm việc cần làm'
+        elemAfterInput={<Button onClick={onAddBtnClick} isDisabled={!textInput} appearance={'primary'}>
+                          Add
+                        </Button>}>
+      </Textfield>
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
